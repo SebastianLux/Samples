7171c8e90147f5e408f2f5a56162eecc0fcae75c
Sebastian Lux <sebastian.lux@live.com>
2014-02-25 20:27:54 +0100

    Sample: HowToCreateANugetPackage

---
+++ HowToCreateANugetPackage/ExtendedMathLib/MyExtendedMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLib;
+
+namespace ExtendedMathLib
+{
+    public class MyExtendedMath : MyMath
+    {
+        public void Test()
+        {
+            
+        }
+    }
+}
