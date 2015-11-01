67f4f66f098ad155b9cee1330481c0829f7cfd70
Sebastian Lux <sebastian.lux@live.com>
2015-11-01 12:08:32 +0100

    Add Crc32 checksum calculation

---
+++ slux.Security.Cryptography/slux.Security.Cryptography.Tests/Crc32Tests.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace slux.Security.Cryptography.Tests
+{
+    [TestClass]
+    public class Crc32Tests
+    {
+        [TestMethod]
+        public void ShouldDifferOnBitSwitch()
+        {
+            var crc32 = new Crc32();
+
+            var crc32Result = crc32.ComputeHash(Encoding.Default.GetBytes("00001"));
+
+            var crc32TwoResult = crc32.ComputeHash(Encoding.Default.GetBytes("00010"));
+
+            CollectionAssert.AreNotEqual(crc32Result, crc32TwoResult);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSameHashForMultipleCalls()
+        {
+            var crc32 = new Crc32();
+
+            byte[] previous = null;
+
+            for (int i = 0; i < 100; i++)
+            {
+                var actual = crc32.ComputeHash(
+                    Encoding.Default.GetBytes("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta ex quis volutpat hendrerit. Vivamus consequat venenatis velit sit amet varius."));
+
+                if (previous != null)
+                {
+                    CollectionAssert.AreEqual(previous, actual);            
+                }
+
+                previous = actual;
+            }
+        }
+    }
+}
---
+++ slux.Security.Cryptography/slux.Security.Cryptography/Crc32.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace slux.Security.Cryptography
+{
+    public sealed class Crc32 : HashAlgorithm
+    {
+        private UInt32 hash;
+        private readonly UInt32 polynomial = 0xedb88320;
+        private readonly UInt32[] table;
+
+        public Crc32()
+        {
+            this.table = InitializeTable(); 
+        }
+
+        public override void Initialize()
+        {
+            // Not implemented.
+        }
+
+
+        protected override void HashCore(byte[] buffer, int start, int length)
+        {
+            this.hash = Calculate(buffer, start, length);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            return new byte[] {
+					(byte)((this.hash >> 24) & 0xff),
+					(byte)((this.hash >> 16) & 0xff),
+					(byte)((this.hash >> 8) & 0xff),
+					(byte)(this.hash & 0xff)
+				};
+        }
+
+        public override int HashSize
+        {
+            get { return 32; }
+        }
+
+        private UInt32[] InitializeTable()
+        {
+            var createTable = new UInt32[256];
+
+            for (var i = 0; i < 256; i++)
+            {
+                var entry = (UInt32)i;
+
+                for (var j = 0; j < 8; j++)
+                {
+                    if ((entry & 1) == 1)
+                        entry = (entry >> 1) ^ this.polynomial;
+                    else
+                        entry = entry >> 1;
+                }
+
+                createTable[i] = entry;
+            }
+
+            return createTable;
+        }
+
+        private UInt32 Calculate(byte[] buffer, int start, int size)
+        {
+            var crc = 0xffffffff;
+
+            for (var i = start; i < size; i++)
+            {
+                unchecked
+                {
+                    crc = (crc >> 8) ^ table[buffer[i] ^ crc & 0xff];
+                }
+            }
+
+            return ~crc;
+        }
+    }
+}
