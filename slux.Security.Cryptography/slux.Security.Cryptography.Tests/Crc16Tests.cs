c9ca9ebbe38a38b918b94c410850fde07cb48d1f
Sebastian Lux <sebastian.lux@live.com>
2016-11-27 17:56:35 +0100

    Add Crc16

---
+++ slux.Security.Cryptography/slux.Security.Cryptography.Tests/Crc16Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace slux.Security.Cryptography.Tests
+{
+    [TestClass]
+    public class Crc16Tests
+    {
+        [TestMethod]
+        public void ShouldComputeCrc16()
+        {
+            var crc16 = new Crc16();
+
+            // ISO/IEC13239: This should lead to the CRC 0x52ED
+            byte[] source =
+            {
+                0x02, 0x07, 0x01, 0x03, 0x01, 0x02, 0x00, 0x34, 0x07, 0x07, 0x1C, 0x59, 0x34, 0x6F, 0xE1,
+                0x83, 0x00, 0x00, 0x41, 0x06, 0x06, 0x7B, 0x3C, 0xFF, 0xCF, 0x3C, 0xC0
+            };
+
+            var hash1 = crc16.ComputeHash(source);
+
+            Assert.AreEqual("52ED", BitConverter.ToString(hash1).Replace("-", ""));
+        }
+
+    }
+}
---
+++ slux.Security.Cryptography/slux.Security.Cryptography/Crc16.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace slux.Security.Cryptography
+{
+    public class Crc16 : HashAlgorithm
+    {
+        private readonly object syncLock = new object();
+
+        // ISO/IEC13239 CRC16
+        private readonly UInt16 polynomial = 0x8408;
+        private UInt16 hash;
+        private readonly UInt16[] table;
+
+
+        public Crc16()
+        {
+            this.table = InitializeTable();
+        }
+
+        public override void Initialize()
+        {
+          
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            //ushort crc = 0xFFFF;
+            //for (int i = 0; i < array.Length; ++i)
+            //{
+            //    byte index = (byte)(crc ^ array[i]);
+            //    crc = (ushort)((crc >> 8) ^ table[index]);
+            //}
+            //this.hash = (ushort) ~crc;
+
+            //ushort fcs = 0xFFFF;
+
+            //int end = ibStart + cbSize;
+
+            //for (int i = ibStart; i < end; i++)
+            //{
+            //    fcs = (ushort)(((ushort)(fcs >> 8)) ^ table[(fcs ^ array[i]) & 0xFF]);
+            //}
+
+            //this.hash = (ushort) ~fcs;
+            unchecked
+            {
+                ushort computedHash = (ushort) -1;
+
+
+
+                for (var i = ibStart; i < cbSize; i++)
+                {
+                    unchecked
+                    {
+                        computedHash = (ushort) ((computedHash >> 8) ^ this.table[array[i] ^ computedHash & 0xff]);
+                    }
+                }
+
+                this.hash = (ushort) ~computedHash;
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            lock (this.syncLock)
+            {
+                return new byte[]
+                       {
+                           (byte) ((this.hash >> 8) & 0xff),
+                           (byte) (this.hash & 0xff)
+                       };
+            }
+        }
+
+        public UInt16[] InitializeTable()
+        {
+            var createTable = new UInt16[256];
+
+            for (var i = 0; i < 256; i++)
+            {
+                var entry = (UInt16)i;
+
+                for (var j = 0; j < 8; j++)
+                {
+                    if ((entry & 1) == 1)
+                        entry = (UInt16) ((entry >> 1) ^ this.polynomial);
+                    else
+                        entry = (UInt16) (entry >> 1);
+                }
+
+                createTable[i] = entry;
+            }
+
+            return createTable;
+        }
+    }
+}
